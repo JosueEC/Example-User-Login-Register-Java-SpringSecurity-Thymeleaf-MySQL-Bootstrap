6b4a2be6e2ed486d7056fbe915ada8e6b1eb9158
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-03 11:15:15 -0600

    feat(models): Creamos la relacion @ManyToMany entre User y Rol
    Definimos que 1 usuario pude tener asignados varios roles y un rol puede estar asignado a varios usuarios

--- src/main/java/com/examplesspring/apploginregisterusers/models/User.java
+++ src/main/java/com/examplesspring/apploginregisterusers/models/User.java
@@ -27,6 +27,16 @@ public class User {
 	private String email;
 	@Column (name = "password")
 	private String password;
+	
+	@ManyToMany (
+			fetch = FetchType.EAGER,
+			cascade = CascadeType.ALL
+	)
+	@JoinTable (
+			name = "users_roles",
+			joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+			inverseJoinColumns = @JoinColumn(name = "rol_id", referencedColumnName = "id")
+	)
 	private Collection<Rol> roles;
 	
 	public User() {}
