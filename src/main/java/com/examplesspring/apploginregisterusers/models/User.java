a9106c43de3320f6201c7ae0809345354ec3d908
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-03 11:06:34 -0600

    feat(models): Definicion de Getters, Setters, Constructor en User
    Definimos las funciones Getters y Setters de cada propiedad de la entidad User, asi como un constructor vacio y el constructor que crea las instancias

--- src/main/java/com/examplesspring/apploginregisterusers/models/User.java
+++ src/main/java/com/examplesspring/apploginregisterusers/models/User.java
@@ -27,5 +27,76 @@ public class User {
 	private String email;
 	@Column (name = "password")
 	private String password;
+	private Collection<Rol> roles;
 	
+	public User() {}
+	
+	public User(Long id, String name, String last_name, String email, String password, Collection<Rol> roles) {
+		this.id = id;
+		this.name = name;
+		this.last_name = last_name;
+		this.email = email;
+		this.password = password;
+		this.roles = roles;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getLast_name() {
+		return last_name;
+	}
+	
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public Collection<Rol> getRoles() {
+		return roles;
+	}
+	
+	public void setRoles(Collection<Rol> roles) {
+		this.roles = roles;
+	}
+	
+	@Override
+	public String toString() {
+		return "User{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", last_name='" + last_name + '\'' +
+				", email='" + email + '\'' +
+				", password='" + password + '\'' +
+				", roles=" + roles +
+				'}';
+	}
 }
