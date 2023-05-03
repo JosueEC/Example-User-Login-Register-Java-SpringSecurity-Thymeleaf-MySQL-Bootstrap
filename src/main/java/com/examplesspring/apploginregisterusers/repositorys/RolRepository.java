3e0e1179ee9a3d1558bd9b0834c09068058b6850
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-03 11:35:01 -0600

    feat(repositorys): Creamos la interface RolRepository
    Esta interface nos permitira acceder a la capa de datos para manipular entidades Rol

---
+++ src/main/java/com/examplesspring/apploginregisterusers/repositorys/RolRepository.java
@@ -0,0 +1,7 @@
+package com.examplesspring.apploginregisterusers.repositorys;
+
+import com.examplesspring.apploginregisterusers.models.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol,Long> {
+}
