2b607fe325d8164d95584c6664874a605e00eedf
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-03 11:37:25 -0600

    fix(repositorys): Falto etiqueta @Repository
    En ambas entidades nos falto agregar la etiqueta @Repository, asi que la agregamos a cada una

--- src/main/java/com/examplesspring/apploginregisterusers/repositorys/RolRepository.java
+++ src/main/java/com/examplesspring/apploginregisterusers/repositorys/RolRepository.java
@@ -2,6 +2,8 @@ package com.examplesspring.apploginregisterusers.repositorys;
 
 import com.examplesspring.apploginregisterusers.models.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RolRepository extends JpaRepository<Rol,Long> {
 }
--- src/main/java/com/examplesspring/apploginregisterusers/repositorys/UserRepository.java
+++ src/main/java/com/examplesspring/apploginregisterusers/repositorys/UserRepository.java
@@ -2,6 +2,8 @@ package com.examplesspring.apploginregisterusers.repositorys;
 
 import com.examplesspring.apploginregisterusers.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
