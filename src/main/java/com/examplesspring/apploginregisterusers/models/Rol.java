0c24da2c10cdb276af45d9fa5cc42b4058bec2f4
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-03 11:04:53 -0600

    feat(models): Creacion de la entidad Rol
    Esta entidad define tipos de roles que puede tener un usuario, tiene las propiedades de id y name

---
+++ src/main/java/com/examplesspring/apploginregisterusers/models/Rol.java
@@ -0,0 +1,53 @@
+package com.examplesspring.apploginregisterusers.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "rol")
+public class Rol {
+	@Id
+	@SequenceGenerator (
+			name = "rol_sequence",
+			sequenceName = "rol_sequence",
+			allocationSize = 1,
+			initialValue = 1
+	)
+	@GeneratedValue (
+			strategy = GenerationType.SEQUENCE,
+			generator = "rol_sequence"
+	)
+	private Long id;
+	@Column (name = "name")
+	private String name;
+	
+	public Rol() {}
+	
+	public Rol(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Rol{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				'}';
+	}
+}
